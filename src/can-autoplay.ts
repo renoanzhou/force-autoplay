f17114e777201ce43b5dcf3c69f12d08882ab18b
zhanhong <zhouzhanhong@polyv.net>
2025-07-23 17:04:01 +0800

    修复muted: false， 给video设置muted=false的attribute

--- src/can-autoplay.ts
+++ src/can-autoplay.ts
@@ -25,8 +25,8 @@ function setAttr (dom: HTMLMediaElement, options: CheckOptions) {
     if (key === 'inline' && attr[key]) {
       dom.setAttribute('playsinline', '')
       dom.setAttribute('webkit-playsinline', '')
-    } else if (key === 'muted' && attr[key] === true) {
-      dom.setAttribute('muted', '')
+    } else if (key === 'muted') {
+      attr[key] === true && dom.setAttribute('muted', '')
     } else if (key in dom) {
       dom.setAttribute(key, `${attr[key]}`)
     }
